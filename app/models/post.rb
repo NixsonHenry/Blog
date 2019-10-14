c8df46937714cc7257a07c44a77b0460f150e820
Henry Nixson <henrynixon.hn@gmail.com>
2019-10-14 18:30:32 -0400

    my first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    ## Add an association to the post
+    has_many :comments
+    ## Add some validation to the model
+    validates :title, presence: true, length: {minimum: 5}
+    validates :body, presence: true
 end
